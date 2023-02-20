3d48e668d9281a610d6cce1770f717f6bcf97fe1
audunlk <audunlk@gmail.com>
2023-02-20 10:41:08 +0100

    added changes and tenary

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,15 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "clean-my-react.clean-my-react",
     () => {
-      vscode.window
+      vscode.window.showWarningMessage(
+        "Do you want to keep the CSS files?",
+        "Yes",
+        "No"
+      ).then((selection) => {
+        selection === "Yes" ? keepCSS() : deleteCSS();
+      }
+      );
+      function deleteCSS(){vscode.window
         .showWarningMessage(
           "Are you sure you want to clean up your React App?",
           "Yes",
@@ -137,7 +145,7 @@ export function activate(context: vscode.ExtensionContext) {
               );
               fs.writeFileSync(
                 path.join(directory, "src", "index.js"),
-                `import React from 'react';\nimport ReactDOM from 'react-dom';\nimport App from './App';\n\nReactDOM.render(\n  <React.StrictMode>\n    <App />\n  </React.StrictMode>,\n  document.getElementById('root')\n);`
+                `import React from 'react';\nimport ReactDOM from 'react-dom/client';\nimport App from './App';\n\nconst root = ReactDOM.createRoot(document.getElementById('root'));\nroot.render(\n <React.StrictMode>\n    <App />\n </React.StrictMode>\n);`
               );
             } else {
               console.log(`Could not find the React app in ${searchDirectory}`);
@@ -148,6 +156,135 @@ export function activate(context: vscode.ExtensionContext) {
             vscode.window.showInformationMessage("Aborted!");
           }
         });
+      };
+
+      function keepCSS(){vscode.window
+        .showWarningMessage(
+          "Are you sure you want to clean up your React App?",
+          "Yes",
+          "No"
+        )
+        .then((selection) => {
+          if (selection === "Yes") {
+            const workspaceFolders = vscode.workspace.workspaceFolders;
+            console.log(workspaceFolders + " is the workspaceFolders");
+
+            let searchDirectory: string | undefined;
+
+            if (workspaceFolders && workspaceFolders.length > 0) {
+              searchDirectory = workspaceFolders[0].uri.fsPath;
+            } else {
+              searchDirectory = process.cwd();
+            }
+
+            const files = fs.readdirSync(searchDirectory!);
+            console.log(files + " is the files");
+            const fullPath = path.join(
+              workspaceFolders![0].uri.fsPath,
+              files[0]
+            );
+
+            const directory = fullPath;
+            console.log(directory + " is the directory");
+
+            if (directory) {
+              fs.unlink(
+                path.join(directory, "public", "favicon.ico"),
+                (err) => {
+                  if (err) {
+                    console.error(err);
+                    return;
+                  }
+                }
+              );
+
+              fs.unlink(
+                path.join(directory, "public", "logo192.png"),
+                (err) => {
+                  if (err) {
+                    console.error(err);
+                    return;
+                  }
+                }
+              );
+
+              fs.unlink(
+                path.join(directory, "public", "logo512.png"),
+                (err) => {
+                  if (err) {
+                    console.error(err);
+                    return;
+                  }
+                }
+              );
+
+              fs.unlink(path.join(directory, "public", "robots.txt"), (err) => {
+                if (err) {
+                  console.error(err);
+                  return;
+                }
+              });
+
+              fs.unlink(
+                path.join(directory, "public", "manifest.json"),
+                (err) => {
+                  if (err) {
+                    console.error(err);
+                    return;
+                  }
+                }
+              );
+
+      
+
+              fs.unlink(path.join(directory, "src", "App.test.js"), (err) => {
+                if (err) {
+                  console.error(err);
+                  return;
+                }
+              });
+
+              fs.unlink(
+                path.join(directory, "src", "reportWebVitals.js"),
+                (err) => {
+                  if (err) {
+                    console.error(err);
+                    return;
+                  }
+                }
+              );
+
+              fs.unlink(path.join(directory, "src", "setupTests.js"), (err) => {
+                if (err) {
+                  console.error(err);
+                  return;
+                }
+              });
+
+              fs.unlink(path.join(directory, "src", "logo.svg"), (err) => {
+                if (err) {
+                  console.error(err);
+                  return;
+                }
+              });
+              fs.writeFileSync(
+                path.join(directory, "src", "App.js"),
+                `import React from 'react';\nimport './App.css'\n\nfunction App() {\n  return (\n    <div className="App">\n    	Hello World!\n    </div>\n  );\n}\n\nexport default App;\n`
+              );
+              fs.writeFileSync(
+                path.join(directory, "src", "index.js"),
+                `import React from 'react';\nimport ReactDOM from 'react-dom/client';\nimport './index.css';\nimport App from './App';\n\nconst root = ReactDOM.createRoot(document.getElementById('root'));\nroot.render(\n <React.StrictMode>\n    <App />\n </React.StrictMode>\n);`
+              );
+            } else {
+              console.log(`Could not find the React app in ${searchDirectory}`);
+            }
+
+            vscode.window.showInformationMessage("React app cleaned!");
+          } else if (selection === "No") {
+            vscode.window.showInformationMessage("Aborted!");
+          }
+        });
+      };
     }
   );
 
